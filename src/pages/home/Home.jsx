30b500b61043b88223f564e03e248e7996ee5109
Laura Betancourt <laura.horta@correounivalle.edu.co>
2024-10-19 17:30:14 -0500

    Adding video

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,17 +9,24 @@ const Home = () => {
 
       {/* Sección de bienvenida a la isla */}
       <section className="home-imagery">
-        <img
-          className="home-imagery_image"
-          src="/images/malpelo.jpg"
-          alt="Isla Malpelo"
-        />
+        <div class="home-imagery_video-container">
+          <video
+            className="home-imagery_video"
+            src="/public/videos/home.mp4"
+            alt="Isla Malpelo"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
         <h2 className="home-imagery_welcome-text">Bienvenido a la</h2>
         <h1 className="home-imagery_island-text">Isla Malpelo</h1>
         <span className="home-imagery_location">
           <img
             className="home-imagery_location-icon"
             src="/images/location.png"
+            alt="Icono de ubicación"
           />
         </span>
         <h2 className="home-imagery_location-text">
